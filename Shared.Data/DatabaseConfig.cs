fe444d06e6df71984b4531cac6b69b9e10501d57
dcarranzav <dacarranzav@puce.edu.ec>
2026-01-11 13:45:34 -0500

    Update production settings

--- Shared.Data/DatabaseConfig.cs
+++ Shared.Data/DatabaseConfig.cs
@@ -4,5 +4,5 @@ public static class DatabaseConfig
 {
     // Cadena de conexión a la base de datos SQL Server en Somee.com
     // NOTA: En producción (Railway), esto será sobrescrito por variables de entorno
-    public const string ConnectionString = "Server=db31651.public.databaseasp.net;Database=db31651;User Id=db31651;Password=prueba2020d;Encrypt=True;TrustServerCertificate=True;MultipleActiveResultSets=True;";
+    public const string ConnectionString = "Server=db31701.public.databaseasp.net,1433;Database=db31701;User Id=db31701;Password=T=q7p2?H#tB4;Encrypt=True;TrustServerCertificate=True;";
 }
